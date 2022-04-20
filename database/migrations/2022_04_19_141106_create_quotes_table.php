0118f8d8fd84f4bfff843179d807b2ea6ca6bd81
Hemerson Varela <hemerson.varela@gmail.com>
2022-04-19 22:49:00 -0400

    create migration files

--- database/migrations/2022_04_19_141106_create_quotes_table.php
+++ database/migrations/2022_04_19_141106_create_quotes_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Quote;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,8 +16,10 @@ return new class extends Migration
     {
         Schema::create('quotes', function (Blueprint $table) {
             $table->id();
-
+            $table->bigInteger('quote_id');
+            $table->boolean('is_valid')->default(Quote::IS_NOT_VALID);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
