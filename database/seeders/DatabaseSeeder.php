1e3a2371d3b76e68e672a18f83997189a53e9696
Hemerson Varela <hemerson.varela@gmail.com>
2022-04-19 22:59:53 -0400

    set database seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Carrier;
+use App\Models\Quote;
+use App\Models\QuoteDetail;
+use App\Models\Tariff;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +18,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Carrier::factory(20)->create();
+        Tariff::factory(30)->create();
+        Quote::factory(25)->create();
+        QuoteDetail::factory(50)->create();
     }
 }
