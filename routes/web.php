07f1fb6f1978163e60fedca3c1c67c5f4879ca78
Hemerson Varela <hemerson.varela@gmail.com>
2022-04-20 23:34:59 -0400

    update routes files

--- routes/web.php
+++ routes/web.php
@@ -17,32 +17,5 @@ use Illuminate\Support\Facades\Http;
 */
 
 Route::get('/', function () {
-
-    $response = Http::get('https://run.mocky.io/v3/bf69042f-9261-4313-8e4a-5a00d05f2e7b');
-    $response = strtr(
-        $response->getBody()->getContents(),
-        ['</s:' => '</', '<s:' => '<', '</a:' => '</', '<a:' => '<']
-    );
-    $output = json_decode(json_encode(simplexml_load_string($response)));
-//    var_dump($output->Body->GetQuotesResponse->GetQuotesResult);
-
-
-    dd($output->Body->GetQuotesResponse->GetQuotesResult->Quotes);
-
-    $xml = simplexml_load_string($clean_xml);
-
-    dd($xml);
-
-
-//    dd($response->getBody());
-//
-//    dd(
-//        json_decode(
-//            json_encode(
-//                (array)simplexml_load_string($response->getBody()->getContents())
-//        ),true)
-//    );
-
-
     return view('welcome');
 });
